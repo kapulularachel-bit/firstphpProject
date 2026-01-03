b6c6fd954f48da67f8e9162c23d61f7d00d2d0a4
Brie <carbon3dental@gmail.com>
2026-01-03 01:08:07 -0800

    commit file formhander.inc.php

--- includes/formhander.inc.php
+++ includes/formhander.inc.php
@@ -1,5 +1,24 @@
 <?php
-  
-  if($_SERVER["REQUIRED_METHOD"]==="POST"){
-        $
+
+  if($_SERVER["REQUEST_METHOD"]==="POST"){
+        $fullname = $_POST['customername'];
+        $email = $_POST['email'];   
+        $password = $_POST['pswrd'];
+
+        require_once 'dbh.inc.php';
+
+        $sql = "INSERT INTO customers(customername, email, pswrd) VALUES (:customername, :email, :pswrd)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':customername',$fullname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':pswrd', $password);
+
+        $stmt->execute();
+        echo "New record created successfully";
+
+  }
+
+ else{
+    header("Location: ../page.php?error=invalidrequest");
+    echo "Invalid request method.";
   }
--- page.php
+++ page.php
@@ -10,15 +10,16 @@
     <section>
         <h1>SIGN IN</h1>
         <form action="includes/formhandler.inc.php" method="post">
-          <label for="firstname">First Name:</label><br>
-          <input type="text" name="firstname" id="firstname">
+          <label for="customername">Full Name:</label><br>
+          <input type="text" name="customername" id="customername">
           <br><br>
-          <label for="lastname">Last Name:</label><br>
-          <input type="text" name="lastname" id="lastname">
-          <br><br>
-          <label for="email">Email:</label><br>
+           <label for="email">Email:</label><br>
           <input type="email" name="email" id="email">
           <br><br>
+          <label for="pswrd">password:</label><br>
+          <input type="password" name="pswrd" id="pswrd">
+          <br><br>
+         
           <button type="submit">Sign In</button>
         </form>
     </section>
